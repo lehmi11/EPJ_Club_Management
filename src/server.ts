9814f80af991c3a29b4278313206a166a09eaa89
monzond <dominicmonzon@gmail.com>
2019-03-31 22:27:00 +0200

    ready for simple demo
    moved some files
    readiness for CI
    setup for handlebars

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,38 @@
 import bodyparser from "body-parser";
 import express from "express";
-import hbs from "express-hbs";
+import hbs from "express-handlebars";
 import {Request, Response} from "express-serve-static-core";
 
 
 
 const app = express();
-app.engine("hbs", hbs.express4());
-app.set("view engine", "hbs");
-app.set("views", __dirname + "/views");
+app.engine("handlebars", hbs({defaultLayout: 'main'}));
+app.set("view engine", "handlebars");
+app.use('/public', express.static('public'));
 
-
-
-
-app.get("/", function(req: Request, res: Response) {
-  res.send("Hello EPJ Club Manager!");
+app.get('/', (req, res) => {
+  res.render('login', { title: 'Login', hideSidebar: 'true'});
 });
+
+app.get('/members', (req, res) => {
+  res.render('members', { title: 'Mitglieder'});
+});
+
+app.get('/teams', (req, res) => {
+  res.render('teams'), { title: 'Teams'};
+});
+
+app.get('/events', (req, res) => {
+  res.render('events', { title: 'Teamübersicht'});
+})
+
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard_finance', { title: 'Dashboard'});
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile', { title: 'mein Profil'});
+})
 
 const hostname = "127.0.0.1";
 const port = 3001;
