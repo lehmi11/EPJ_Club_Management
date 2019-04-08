81024b3ced82570f59371cd0c181895c26cbea28
qwykx <Masesa13>
2019-04-08 09:08:33 +0200

    - Fixed "npm start" error (Handlebars precompilation error) - Moved datatables config from "datatables_paymentStatus.js" to "dashboard_finance.js" - Fixed TSLint errors

--- views/js/app/dashboard_finance.js
+++ views/js/app/dashboard_finance.js
@@ -27,7 +27,63 @@ $(document).ready(function() {
 
             $appMembersNotPaid.html(memberTable);
 
-            $('#memberTable').DataTable( window.globalDataTableConf );
+            window.globalDataTableConf = {
+                language: {
+                    "buttons": {
+                        "colvis":   "Spalten",
+                        "copy":     "Kopieren",
+                        "copyKeys": "Taste <i>ctrl</i> oder <i>\u2318</i> + <i>C</i> um Tabelle" +
+                            "<br>in Zwischenspeicher zu kopieren.<br><br>" +
+                            "Um abzubrechen die Nachricht anklicken oder Escape drücken.",
+                        "copySuccess": {
+                            "1": "1 Spalte kopiert",
+                            "_": "%d Spalten kopiert",
+                        },
+                        "copyTitle":    "In Zwischenablage kopieren",
+                        "print": "Drucken",
+                    },
+                    "oAria": {
+                        "sSortAscending":  ": aktivieren, um Spalte aufsteigend zu sortieren",
+                        "sSortDescending": ": aktivieren, um Spalte absteigend zu sortieren",
+                    },
+                    "oPaginate": {
+                        "sFirst":       "Erste",
+                        "sLast":        "Letzte",
+                        "sNext":        "Nächste",
+                        "sPrevious":    "Zurück",
+                    },
+                    "sEmptyTable":      "Keine Daten in der Tabelle vorhanden",
+                    "sInfo":            "_START_ bis _END_ von _TOTAL_ Einträgen",
+                    "sInfoEmpty":       "Keine Daten vorhanden",
+                    "sInfoFiltered":    "(gefiltert von _MAX_ Einträgen)",
+                    "sInfoPostFix":     "",
+                    "sInfoThousands":   ".",
+                    "sLengthMenu":      "_MENU_ Einträge",
+                    "sLoadingRecords":  "Wird geladen ..",
+                    "sProcessing":      "Bitte warten ..",
+                    "sSearch":          "Suchen",
+                    "sZeroRecords":     "Keine Einträge vorhanden",
+                    "select": {
+                        "rows": {
+                            "0": "",
+                            "1": "1 Zeile ausgewählt",
+                            "_": "%d Zeilen ausgewählt",
+                        },
+                    },
+                },
+                "lengthChange": false,
+            };
+
+            // Datatable settings
+            $("#memberTable").DataTable( window.globalDataTableConf );
+
+            // Disable Datatable search field
+            $("#memberTable_filter").hide();
+
+            // Custom search field
+            $("#dataTableSearch").keyup(function() {
+                $("#memberTable").DataTable().search($(this).val()).draw() ;
+            });
         });
     }
 });
