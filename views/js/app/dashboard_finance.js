f622853b755e170921c8ca1efe8c1a16acb06607
qwykx <Masesa13>
2019-04-08 09:33:46 +0200

    - Fixed "npm start" error (Handlebars precompilation error) - Moved datatables config from "datatables_paymentStatus.js" to "dashboard_finance.js" - Fixed TSLint errors

--- views/js/app/dashboard_finance.js
+++ views/js/app/dashboard_finance.js
@@ -27,31 +27,10 @@ $(document).ready(function() {
 
             $appMembersNotPaid.html(memberTable);
 
-            window.globalDataTableConf = {
+            // Datatable settings
+            $("#memberTable").DataTable( {
+                "lengthChange": false,
                 language: {
-                    "buttons": {
-                        "colvis":   "Spalten",
-                        "copy":     "Kopieren",
-                        "copyKeys": "Taste <i>ctrl</i> oder <i>\u2318</i> + <i>C</i> um Tabelle" +
-                            "<br>in Zwischenspeicher zu kopieren.<br><br>" +
-                            "Um abzubrechen die Nachricht anklicken oder Escape drücken.",
-                        "copySuccess": {
-                            "1": "1 Spalte kopiert",
-                            "_": "%d Spalten kopiert",
-                        },
-                        "copyTitle":    "In Zwischenablage kopieren",
-                        "print": "Drucken",
-                    },
-                    "oAria": {
-                        "sSortAscending":  ": aktivieren, um Spalte aufsteigend zu sortieren",
-                        "sSortDescending": ": aktivieren, um Spalte absteigend zu sortieren",
-                    },
-                    "oPaginate": {
-                        "sFirst":       "Erste",
-                        "sLast":        "Letzte",
-                        "sNext":        "Nächste",
-                        "sPrevious":    "Zurück",
-                    },
                     "sEmptyTable":      "Keine Daten in der Tabelle vorhanden",
                     "sInfo":            "_START_ bis _END_ von _TOTAL_ Einträgen",
                     "sInfoEmpty":       "Keine Daten vorhanden",
@@ -63,24 +42,39 @@ $(document).ready(function() {
                     "sProcessing":      "Bitte warten ..",
                     "sSearch":          "Suchen",
                     "sZeroRecords":     "Keine Einträge vorhanden",
+                    "oPaginate": {
+                        "sFirst":       "Erste",
+                        "sPrevious":    "Zurück",
+                        "sNext":        "Nächste",
+                        "sLast":        "Letzte"
+                    },
+                    "oAria": {
+                        "sSortAscending":  ": aktivieren, um Spalte aufsteigend zu sortieren",
+                        "sSortDescending": ": aktivieren, um Spalte absteigend zu sortieren"
+                    },
                     "select": {
                         "rows": {
-                            "0": "",
-                            "1": "1 Zeile ausgewählt",
                             "_": "%d Zeilen ausgewählt",
-                        },
+                            "0": "",
+                            "1": "1 Zeile ausgewählt"
+                        }
                     },
-                },
-                "lengthChange": false,
-            };
-
-            // Datatable settings
-            $("#memberTable").DataTable( window.globalDataTableConf );
-
-            // Disable Datatable search field
-            $("#memberTable_filter").hide();
+                    "buttons": {
+                        "print":    "Drucken",
+                        "colvis":   "Spalten",
+                        "copy":     "Kopieren",
+                        "copyTitle":    "In Zwischenablage kopieren",
+                        "copyKeys": "Taste <i>ctrl</i> oder <i>\u2318</i> + <i>C</i> um Tabelle<br>in Zwischenspeicher zu kopieren.<br><br>Um abzubrechen die Nachricht anklicken oder Escape drücken.",
+                        "copySuccess": {
+                            "_": "%d Spalten kopiert",
+                            "1": "1 Spalte kopiert"
+                        }
+                    }
+                }
+            });
 
             // Custom search field
+            $("#memberTable_filter").hide();
             $("#dataTableSearch").keyup(function() {
                 $("#memberTable").DataTable().search($(this).val()).draw() ;
             });
