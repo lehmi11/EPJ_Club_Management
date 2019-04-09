94bb4627a457e63e4c1183eace1d5be75df23edf
Alex Handlebar <alexander.steiner@hsr.ch>
2019-04-09 15:45:04 +0200

    get Member über typeorm wieder eingefügt

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -5,5 +5,6 @@ import {apiController} from "../controllers/apiController";
 router.get("/dashboard", apiController.getFinanceDashboard.bind(apiController));
 router.get("/members/notpaid", apiController.getMembersNotPaid.bind(apiController));
 router.get("/members/paymentStatus", apiController.getMembersPaymentStatus.bind(apiController));
+router.get("/members", apiController.getMembers.bind(apiController));
 
 export const apiRoutes = router;
