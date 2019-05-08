2d19088c17d8e1274c84da0a9b68a5f6c1cad39f
lehmi11 <patrick.joel.lehmann@hsr.ch>
2019-05-08 12:05:41 +0200

    add Sinon for Integration Tests

---
+++ test/integration_test.ts
@@ -0,0 +1,14 @@
+import { hello } from "../src/hello";
+import { expect } from "chai";
+import { Sinon } from "sinon";
+
+import 'mocha';
+
+describe('Hello function', () => {
+
+  it('should return hello world', () => {
+    const result = hello();
+    expect(result).to.equal('Hello world!');
+  });
+
+});
